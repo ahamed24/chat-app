22ed82fc7ba234beab3969daa313c2652b753299
ahamed24 <ahamednoordc@gmail.com>
2025-02-21 08:38:48 +0530

    updated the code

--- backend/main.go
+++ backend/main.go
@@ -8,6 +8,15 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+type ChatMessage struct {
+	Username string `json:"username"`
+	Text     string `json:"text"`
+}
+
+var broadcaster = make(chan ChatMessage)
+
+var clients = make(map[*websocket.Conn]bool)
+
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
@@ -16,33 +25,53 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "hello")
 }
 
-func websocketHandler(w http.ResponseWriter, r *http.Request) {
+func handleConnections(w http.ResponseWriter, r *http.Request) {
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Printf("websocket Upgrade error: %v", err)
 		return
 	}
-	defer ws.Close()
 
-	messageType, message, err := ws.ReadMessage()
-	if err != nil {
-		log.Println("Read error:", err)
-		return
+	clients[ws] = true
+	log.Println("New client connected")
+
+	for {
+		var msg ChatMessage
+
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Println("Error reading the message: ", err)
+			delete(clients, ws)
+			break
+		}
+
+		broadcaster <- msg
 	}
 
-	fmt.Println("Received: ", string(message))
+}
 
-	err = ws.WriteMessage(messageType, message)
-	if err != nil {
-		fmt.Println("write error: ", err)
-		return
+func handleMessages() {
+	for {
+		msg := <-broadcaster
+
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Println("Error writing the message: ", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
 	}
 }
 
 func main() {
+
 	http.HandleFunc("/", handler)
 
-	http.HandleFunc("/ws", websocketHandler)
+	http.HandleFunc("/ws", handleConnections)
+
+	go handleMessages()
 
 	fmt.Println("Server running on port: 8080")
 	err := http.ListenAndServe(":8080", nil)
